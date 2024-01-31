3f1d2ab5e74406a7c02d21d31a57cc77eb2750ce
tdora28 <tokaidora88@gmail.com>
2024-01-31 15:45:54 +0200

    Add initial solution for showing cart items on Cart page

--- src/routes/Cart.jsx
+++ src/routes/Cart.jsx
@@ -1,7 +1,16 @@
+import { useSelector } from 'react-redux';
+import ProductSingle from './ProductSingle';
+
 const Cart = () => {
+  const { cart } = useSelector((state) => state.cart);
+
+  console.log(cart);
+
   return (
-    <div>
-      <h1>Cart</h1>
+    <div className="products-grid">
+      {cart.map((product) => (
+        <ProductSingle key={product.id} {...product}></ProductSingle>
+      ))}
     </div>
   );
 };
